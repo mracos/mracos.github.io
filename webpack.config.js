9962e51919faf1eed7540bdf078e6e5d98c94864
Marcos Ferreira <marcosp4f@gmail.com>
2018-04-23 23:21:10 -0300

    Add of base webpack app with pug and sass

---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+'use strict'
+
+var path = require('path')
+var webpack = require('webpack')
+
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HtmlHardDiskPlugin = require('html-webpack-harddisk-plugin')
+var Autoprefixer = require('autoprefixer')
+
+function join(dest) {
+  return path.resolve(__dirname, dest)
+}
+
+module.exports = {
+  entry: {
+    app: [
+      join('src/index.pug'),
+      join('src/sass/main.sass'),
+    ],
+ },
+
+  output: {
+    path: join('dist'),
+    publicPath: '/',
+  },
+
+  resolve: {
+    alias: {
+      '@': join('src'),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [Autoprefixer],
+            },
+          },
+          'sass-loader?indentedSyntax',
+        ],
+      },
+      {
+        test: /\.pug$/,
+        use: [
+          'html-loader',
+          'pug-html-loader',
+        ],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            outputPath: 'images',
+          },
+        }],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: '[id].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: join('src/index.pug'),
+      alwaysWriteToDisk: true,
+    }),
+    new HtmlHardDiskPlugin(),
+  ],
+
+  devServer: {
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+    historyApiFallback: true,
+    stats: 'errors-only',
+    overlay: {
+      errors: true,
+      warnings: false
+    },
+  },
+}
